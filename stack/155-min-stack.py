f7cb52e54db1cd2d0ced8b03b1cb02d170075c7f
Paripás Viktor <viktor.paripas@kognity.com>
2024-02-04 00:44:19 +0100

    Solve 155. Min-Stack

---
+++ stack/155-min-stack.py
@@ -0,0 +1,53 @@
+"""
+155. Min Stack
+---
+Difficulty: Medium
+
+Design a stack that supports push, pop, top, and retrieving the minimum
+element in constant time.
+
+Implement the MinStack class:
+- MinStack() initializes the stack object.
+- void push(int val) pushes the element val onto the stack.
+- void pop() removes the element on the top of the stack.
+- int top() gets the top element of the stack.
+- int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+"""
+
+
+class MinStack:
+    def __init__(self):
+        self._stack = []
+        self._min_stack = []
+
+    def push(self, val: int) -> None:
+        self._stack.append(val)
+        if not self._min_stack or val <= self._min_stack[-1]:
+            self._min_stack.append(val)
+
+    def pop(self) -> None:
+        if self._stack.pop() == self._min_stack[-1]:
+            self._min_stack.pop()
+
+    def top(self) -> int:
+        return self._stack[-1]
+
+    def getMin(self) -> int:
+        return self._min_stack[-1]
+
+
+def test_min_stack():
+    min_stack = MinStack()
+    min_stack.push(-2)
+    min_stack.push(0)
+    min_stack.push(-3)
+    assert min_stack.getMin() == -3
+
+    min_stack.pop()
+    assert min_stack.top() == 0
+    assert min_stack.getMin() == -2
+
+
+if __name__ == '__main__':
+    test_min_stack()
