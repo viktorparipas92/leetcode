d7dd7ee406c9fb9ffb5b7a2e1cfde30f7779eadd
Paripás Viktor <viktor.paripas@kognity.com>
2025-01-23 05:18:39 +0100

    Solve 746. Min-cost climbing stairs with recursion

---
+++ dynamic_programming/746_min_cost_climbing_stairs.py
@@ -0,0 +1,66 @@
+"""
+746. Min Cost Climbing Stairs
+---------------------------------
+Difficulty: Easy
+
+You are given an array of integers cost where cost[i] is the cost of taking a step
+from the ith floor of a staircase.
+After paying the cost, you can step to either the (i + 1)th floor or the (i + 2)th floor.
+
+You may choose to start at the index 0 or the index 1 floor.
+
+Return the minimum cost to reach the top of the staircase,
+i.e. just past the last index in cost.
+
+Example 1:
+Input: cost = [1,2,3]
+Output: 2
+Explanation: We can start at index = 1 and pay the cost of cost[1] = 2 and take two
+steps to reach the top. The total cost is 2.
+
+Example 2:
+Input: cost = [1,2,1,2,1,1,1]
+Output: 4
+Explanation: Start at index = 0.
+
+Pay the cost of cost[0] = 1 and take two steps to reach index = 2.
+Pay the cost of cost[2] = 1 and take two steps to reach index = 4.
+Pay the cost of cost[4] = 1 and take two steps to reach index = 6.
+Pay the cost of cost[6] = 1 and take one step to reach the top.
+The total cost is 4.
+Constraints:
+2 <= cost.length <= 100
+0 <= cost[i] <= 100
+"""
+
+
+def min_cost_climbing_stairs_recursion(cost: list[int]) -> int:
+    """
+    Time complexity: O(2^n), space complexity: O(n)
+    """
+    def min_cost(i: int) -> int:
+        if i >= len(cost):
+            return 0
+
+        return cost[i] + min(min_cost(i + 1), min_cost(i + 2))
+
+    return min(min_cost(0), min_cost(1))
+
+
+def test_min_cost_climbing_stairs():
+    solutions = [
+        min_cost_climbing_stairs_recursion,
+    ]
+
+    test_cases = [
+        ([1, 2, 3], 2),
+        ([1, 2, 1, 2, 1, 1, 1], 4),
+    ]
+    for solution in solutions:
+        for cost, expected in test_cases:
+            assert solution(cost) == expected
+        print(f'{solution.__name__} passed all test cases')
+
+
+if __name__ == '__main__':
+    test_min_cost_climbing_stairs()
