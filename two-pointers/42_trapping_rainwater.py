fd6e1f7a665164e2f0d5f6b3af84d9d86c0615fd
Paripás Viktor <viktor.paripas@kognity.com>
2025-01-25 23:20:06 +0100

    Solve 42. Trapping rainwater with brute force

---
+++ two-pointers/42_trapping_rainwater.py
@@ -0,0 +1,65 @@
+"""
+42. Trapping Rain Water
+-----------------------
+Difficulty: Hard
+
+You are given an array non-negative integers height which represent an elevation map.
+Each value height[i] represents the height of a bar, which has a width of 1.
+
+Return the maximum area of water that can be trapped between the bars.
+
+Example 1:
+Input: height = [0,2,0,3,1,0,1,3,2,1]
+Output: 9
+Constraints:
+
+1 <= height.length <= 1000
+0 <= height[i] <= 1000
+"""
+
+
+def trap_brute_force(heights: list[int]) -> int:
+    """
+    Time complexity: O(n^2), space complexity: O(1)
+    """
+    length = len(heights)
+    total_water_area = 0
+
+    for i, height in enumerate(heights):
+        left_max = right_max = height
+        for j in range(i):
+            left_max = max(left_max, heights[j])
+
+        for j in range(i + 1, length):
+            right_max = max(right_max, heights[j])
+
+        total_water_area += min(left_max, right_max) - height
+
+    return total_water_area
+
+
+def test_trap_rainwater():
+    # Arrange
+    solutions = [
+        trap_brute_force,
+    ]
+    test_cases = [
+        ([0, 2, 0, 3, 1, 0, 1, 3, 2, 1], 9),
+        ([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6),
+        ([4, 2, 0, 3, 2, 5], 9),
+    ]
+
+    for solution in solutions:
+        for heights, expected_area in test_cases:
+            # Act
+            area = solution(heights)
+
+            # Assert
+            assert area == expected_area
+
+        print(f"Passed all test cases for {solution.__name__}!")
+
+
+if __name__ == "__main__":
+    test_trap_rainwater()
+
