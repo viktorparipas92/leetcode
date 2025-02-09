3b8a72f000b27595f09725132618edf58716dea7
Paripás Viktor <viktor.paripas@kognity.com>
2025-02-09 09:27:04 +0100

    Add alternative solutions for 190.

--- bit_manipulation/190_reverse_bits.py
+++ bit_manipulation/190_reverse_bits.py
@@ -16,13 +16,16 @@ gives us 10101000000000000000000000000000 which represents the unsigned integer
 """
 
 
+SIZE = 32  # 32-bit integer
+
+
 def reverse_bits_bruce_force(n: int) -> int:
     """
     Time complexity: O(1)
     Space complexity: O(1)
     """
     binary: str = ''
-    for i in range(32):
+    for i in range(SIZE):
         # (1 << i) creates a mask with only the i-th bit set to 1
         # Performing bitwise AND with 'n' isolates this bit.
         binary_increment = '1' if (n & (1 << i)) else '0'
@@ -37,10 +40,39 @@ def reverse_bits_bruce_force(n: int) -> int:
     return reversed_number
 
 
+def reverse_bits_bit_manipulation(n: int) -> int:
+    """
+    Time complexity: O(1)
+    Space complexity: O(1)
+    """
+    reversed_number = 0
+    for i in range(SIZE):
+        bit = (n >> i) & 1  # Extract the i-th bit
+        reversed_number += (bit << (SIZE - 1 - i))  # Set the i-th bit in the reverse
+
+    return reversed_number
+
+
+def reverse_bits_bit_manipulation_optimal(n: int) -> int:
+    """
+    Time complexity: O(1)
+    Space complexity: O(1)
+    """
+    res = n
+    res = (res >> 16) | (res << 16) & 0xFFFFFFFF
+    res = ((res & 0xff00ff00) >> 8) | ((res & 0x00ff00ff) << 8)
+    res = ((res & 0xf0f0f0f0) >> 4) | ((res & 0x0f0f0f0f) << 4)
+    res = ((res & 0xcccccccc) >> 2) | ((res & 0x33333333) << 2)
+    res = ((res & 0xaaaaaaaa) >> 1) | ((res & 0x55555555) << 1)
+    return res & 0xFFFFFFFF
+
+
 def test_reverse_bits():
     # Arrange
     solutions = [
         reverse_bits_bruce_force,
+        reverse_bits_bit_manipulation,
+        reverse_bits_bit_manipulation_optimal,
     ]
 
     test_cases = [
