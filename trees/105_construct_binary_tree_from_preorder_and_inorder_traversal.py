d6d7ad73e72ceb7223d3e436a7f3868bc35411c2
Paripás Viktor <viktor.paripas@kognity.com>
2025-02-17 18:13:42 +0100

    Solve 105. construct BT from preorder and inorder traversals

---
+++ trees/105_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -0,0 +1,71 @@
+"""
+105. Construct Binary Tree from Preorder and Inorder Traversal
+--------------------------------------------------------------
+Difficulty: Medium
+
+You are given two integer arrays preorder and inorder.
+- preorder is the preorder traversal of a binary tree
+- inorder is the inorder traversal of the same tree
+Both arrays are of the same size and consist of unique values.
+Rebuild the binary tree from the preorder and inorder traversals and return its root.
+
+Example 1:
+Input: preorder = [1,2,3,4], inorder = [2,1,3,4]
+Output: [1,2,3,null,null,null,4]
+
+Example 2:
+Input: preorder = [1], inorder = [1]
+Output: [1]
+
+Constraints:
+1 <= inorder.length <= 1000.
+inorder.length == preorder.length
+-1000 <= preorder[i], inorder[i] <= 1000
+"""
+
+from trees.shared import TreeNode as Node
+
+
+def build_tree_dfs(preorder: list[int], inorder: list[int]) -> Node | None:
+    """
+    Time complexity: O(n^2)
+    Space complexity: O(n)
+    """
+    if not preorder or not inorder:
+        return
+
+    root_value = preorder[0]
+    root_node = Node(root_value)
+    mid_index = inorder.index(root_value)
+    root_node.left = build_tree_dfs(preorder[1: mid_index+1], inorder[:mid_index])
+    root_node.right = build_tree_dfs(preorder[mid_index+1:], inorder[mid_index + 1:])
+    return root_node
+
+
+def test_build_tree():
+    solutions = [
+        build_tree_dfs,
+    ]
+
+    tree_1 = Node(1, Node(2), Node(3, None, Node(4)))
+    tree_3 = Node(3, Node(9), Node(20, Node(15), Node(7)))
+    test_cases = [
+        ([1, 2, 3, 4], [2, 1, 3, 4], tree_1),
+        ([1], [1], Node(1)),
+        ([3, 9, 20, 15, 7], [9, 3, 15, 20, 7], tree_3),
+    ]
+
+    for solution in solutions:
+        for preorder, inorder, expected_tree in test_cases:
+            # Act
+            tree = solution(preorder, inorder)
+
+            # Assert
+            assert list(tree.preorder()) == preorder
+            assert list(tree.inorder()) == inorder
+
+        print(f'All tests passed for {solution.__name__}!')
+
+
+if __name__ == '__main__':
+    test_build_tree()
--- trees/shared.py
+++ trees/shared.py
@@ -4,6 +4,21 @@ class TreeNode(object):
         self.left = left
         self.right = right
 
+    def inorder(self):
+        if self.left:
+            yield from self.left.inorder()
+        yield self.value
+        if self.right:
+            yield from self.right.inorder()
+
+    def preorder(self):
+        yield self.value
+        if self.left:
+            yield from self.left.preorder()
+
+        if self.right:
+            yield from self.right.preorder()
+
 
 def is_same_tree(p: TreeNode, q: TreeNode) -> bool:
     if p is None and q is None:
