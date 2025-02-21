e9915e5d8c8d702fda3a210e880b0467a91a422b
Paripás Viktor <viktor.paripas@kognity.com>
2025-02-21 23:34:14 +0100

    Solve 40. Combination Sum II with brute-force

---
+++ backtracking/40_combination_sum_ii.py
@@ -0,0 +1,89 @@
+"""
+40. Combination Sum II
+----------------------------------
+Difficulty: Medium
+
+You are given an array of integers candidates, which may contain duplicates,
+and a target integer target.
+Your task is to return a list of all unique combinations of candidates where the chosen
+numbers sum to target.
+
+Each element from candidates may be chosen at most once within a combination.
+The solution set must not contain duplicate combinations.
+
+You may return the combinations in any order and the order of the numbers in each
+combination can be in any order.
+
+Example 1:
+Input: candidates = [9,2,2,4,6,1,5], target = 8
+Output: [
+  [1,2,5],
+  [2,2,4],
+  [2,6]
+]
+
+Example 2:
+Input: candidates = [1,2,3,4,5], target = 7
+Output: [
+  [1,2,4],
+  [2,5],
+  [3,4]
+]
+
+Constraints:
+1 <= candidates.length <= 100
+1 <= candidates[i] <= 50
+1 <= target <= 30
+"""
+
+
+def combination_sum_2_brute_force(candidates: list[int], target: int) -> list[list[int]]:
+    """
+    Time complexity: O(n 2^n)
+    Space complexity: O(n 2^n)
+    """
+    combinations: set[tuple] = set()
+    candidates.sort()
+
+    def generate_subsets(i, current_combination, total):
+        if total == target:
+            combinations.add(tuple(current_combination))
+            return
+        elif total > target or i == len(candidates):
+            return
+
+        candidate = candidates[i]
+        current_combination.append(candidate)
+        generate_subsets(i + 1, current_combination, total + candidate)
+
+        current_combination.pop()
+        generate_subsets(i + 1, current_combination, total)
+
+    generate_subsets(0, current_combination=[], total=0)
+    return [list(combination) for combination in combinations]
+
+
+def test_combination_sum_2():
+    solutions = [
+        combination_sum_2_brute_force,
+    ]
+
+    test_cases = [
+        ([9, 2, 2, 4, 6, 1, 5], 8, [[1, 2, 5], [2, 2, 4], [2, 6]]),
+        ([1, 2, 3, 4, 5], 7, [[1, 2, 4], [2, 5], [3, 4]]),
+    ]
+
+    for solution in solutions:
+        for candidates, target, expected_combinations in test_cases:
+            # Act
+            combinations = solution(candidates, target)
+
+            # Assert
+            assert len(combinations) == len(expected_combinations)
+
+        print(f'All tests passed for {solution.__name__}!')
+
+
+if __name__ == '__main__':
+    test_combination_sum_2()
+    
