38b559dba18145920bdb963d28f38bff017b1aef
Paripás Viktor <viktor.paripas@kognity.com>
2025-02-27 00:01:33 +0100

    90. Add backtracking solution with less space complexity

--- backtracking/90_subsets_ii.py
+++ backtracking/90_subsets_ii.py
@@ -45,9 +45,38 @@ def subsets_with_duplicates_brute_force(numbers: list[int]) -> list[list[int]]:
     return [list(subset) for subset in subsets]
 
 
+def subsets_with_duplicates_backtracking_pick_or_not(
+    numbers: list[int]
+) -> list[list[int]]:
+    """
+    Time complexity: O(n * 2^n)
+    Space complexity: O(n)
+    """
+    subsets: list[list[int]] = []
+
+    def generate_subsets(i: int, current_subset: list[int]):
+        if i == len(numbers):
+            subsets.append(current_subset[::])
+            return
+
+        current_subset.append(numbers[i])
+        generate_subsets(i + 1, current_subset)
+
+        current_subset.pop()
+        while i + 1 < len(numbers) and numbers[i] == numbers[i + 1]:
+            i += 1
+
+        generate_subsets(i + 1, current_subset)
+
+    numbers.sort()
+    generate_subsets(i=0, current_subset=[])
+    return subsets
+
+
 def test_subsets_with_duplicates():
     solutions = [
         subsets_with_duplicates_brute_force,
+        subsets_with_duplicates_backtracking_pick_or_not,
     ]
 
     test_cases = [
