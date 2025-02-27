2585796f7e7b2ece61439d4a878e45369657ff18
Paripás Viktor <viktor.paripas@kognity.com>
2025-02-27 23:57:06 +0100

    Add problem statement for 53.

---
+++ greedy/53_maximum_subarray.py
@@ -0,0 +1,22 @@
+"""
+53. Maximum Subarray
+---------------------
+Difficulty: Medium
+
+Given an array of integers nums, find the subarray with the largest sum and
+return the sum.
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+Example 1:
+Input: nums = [2,-3,4,-2,2,1,-1,4]
+Output: 8
+Explanation: The subarray [4,-2,2,1,-1,4] has the largest sum 8.
+
+Example 2:
+Input: nums = [-1]
+Output: -1
+
+Constraints:
+1 <= nums.length <= 1000
+-1000 <= nums[i] <= 1000
+"""
