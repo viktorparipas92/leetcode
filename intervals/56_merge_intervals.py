ef152c29db423c85ed588f1f0007f41e325cafdb
Paripás Viktor <viktor.paripas@kognity.com>
2025-03-21 22:53:34 +0100

    Solve 56. merge intervals with sorting

---
+++ intervals/56_merge_intervals.py
@@ -0,0 +1,73 @@
+"""
+56. Merge Intervals
+-------------------
+Difficulty: Medium
+
+Given an array of intervals where intervals[i] = [start_i, end_i],
+merge all overlapping intervals, and return an array of the non-overlapping intervals
+that cover all the intervals in the input.
+You may return the answer in any order.
+
+Note: Intervals are non-overlapping if they have no common point.
+For example, [1, 2] and [3, 4] are non-overlapping, but [1, 2] and [2, 3] are overlapping.
+
+Example 1:
+Input: intervals = [[1,3],[1,5],[6,7]]
+Output: [[1,5],[6,7]]
+
+Example 2:
+Input: intervals = [[1,2],[2,3]]
+Output: [[1,3]]
+
+Constraints:
+1 <= intervals.length <= 1000
+intervals[i].length == 2
+0 <= start <= end <= 1000
+"""
+from collections import namedtuple
+
+
+Interval = namedtuple('Interval', ['start', 'end'])
+
+
+def merge_sorting(intervals: list[Interval]) -> list[Interval]:
+    """
+    Time complexity: O(nlogn)
+    Space complexity: O(n)
+    """
+    intervals.sort(key=lambda interval: interval.start)
+    merged_intervals = [intervals[0]]
+
+    for start, end in intervals:
+        last_interval = merged_intervals[-1]
+        if start <= last_interval.end:
+            merged_intervals[-1] = last_interval._replace(end=max(last_interval.end, end))
+        else:
+            merged_intervals.append(Interval(start, end))
+
+    return merged_intervals
+
+
+def test_merge():
+    solutions = [
+        merge_sorting,
+    ]
+
+    test_cases = [
+        ([Interval(1, 3), Interval(1, 5), Interval(6, 7)], [Interval(1, 5), Interval(6, 7)]),
+        ([Interval(1, 2), Interval(2, 3)], [Interval(1, 3)]),
+    ]
+
+    for solution in solutions:
+        for intervals, expected_merged_intervals in test_cases:
+            # Act
+            merged_intervals = solution(intervals)
+
+            # Assert
+            assert merged_intervals == expected_merged_intervals
+
+        print(f'Tests passed for {solution.__name__}!')
+
+
+if __name__ == '__main__':
+    test_merge()
