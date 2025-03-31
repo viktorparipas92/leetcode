eb6262fa5f06d794fb2af625fb7a6a456422ad04
Paripás Viktor <viktor.paripas@kognity.com>
2025-03-31 16:01:15 +0200

    Solve 25. by iteration

--- linked_list/25_reverse_nodes_in_groups_of_k.py
+++ linked_list/25_reverse_nodes_in_groups_of_k.py
@@ -31,13 +31,6 @@ def reverse_k_group_recursion(head: ListNode, k: int) -> ListNode:
     Time complexity: O(n)
     Space complexity: O(n/k):
     """
-    def reverse(first_node: ListNode, last_node: ListNode) -> tuple[ListNode, ListNode]:
-        temporary_node = first_node.next
-        first_node.next = last_node
-        last_node = first_node
-        first_node = temporary_node
-        return first_node, last_node
-
     current_node = head
     current_group_size = 0
     while current_node and current_group_size < k:
@@ -55,9 +48,51 @@ def reverse_k_group_recursion(head: ListNode, k: int) -> ListNode:
     return head
 
 
+def reverse(first_node: ListNode, last_node: ListNode) -> tuple[ListNode, ListNode]:
+    temporary_node = first_node.next
+    first_node.next = last_node
+    last_node = first_node
+    first_node = temporary_node
+    return first_node, last_node
+
+
+def reverse_k_group_iteration(head: ListNode, k: int) -> ListNode:
+    """
+    Time complexity: O(n)
+    Space complexity: O(1):
+    """
+    def get_node_k(current_node: ListNode, k: int) -> ListNode:
+        while current_node and k > 0:
+            current_node = current_node.next
+            k -= 1
+
+        return current_node
+
+    dummy_node = ListNode(0, head)
+    node_before_group = dummy_node
+
+    while True:
+        node_k = get_node_k(node_before_group, k)
+        if not node_k:
+            break
+
+        # Reverse the group
+        node_after_group = node_k.next
+        previous_node, current_node = node_k.next, node_before_group.next
+        while current_node != node_after_group:
+            current_node, previous_node = reverse(current_node, previous_node)
+
+        tmp = node_before_group.next
+        node_before_group.next = node_k
+        node_before_group = tmp
+
+    return dummy_node.next
+
+
 def test_reverse_k_group():
     solutions = [
         reverse_k_group_recursion,
+        reverse_k_group_iteration,
     ]
 
     test_cases = [
