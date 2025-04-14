f522f8e89149ce9f959078fadf5fde067cdb6c0c
Paripás Viktor <viktor.paripas@kognity.com>
2025-04-14 23:59:06 +0200

    Create placeholder for solution to 200. Number of Islands

---
+++ graphs/200_number_of_islands.py
@@ -0,0 +1,68 @@
+"""
+200. Number of Islands
+--------------------------------
+Difficulty: Medium
+
+Given a 2D grid grid where '1' represents land and '0' represents water,
+count and return the number of islands.
+
+An island is formed by connecting adjacent lands horizontally or vertically
+and is surrounded by water.
+You may assume water is surrounding the grid (i.e., all the edges are water).
+
+Example 1:
+Input: grid = [
+    ['0','1','1','1','0'],
+    ['0','1','0','1','0'],
+    ['1','1','0','0','0'],
+    ['0','0','0','0','0']
+  ]
+Output: 1
+
+Example 2:
+Input: grid = [
+    ['1','1','0','0','1'],
+    ['1','1','0','0','1'],
+    ['0','0','1','0','0'],
+    ['0','0','0','1','1']
+  ]
+Output: 4
+Constraints:
+
+1 <= grid.length, grid[i].length <= 100
+grid[i][j] is '0' or '1'.
+"""
+
+
+def num_islands_dfs(grid: list[list[str]]) -> int:
+    pass
+
+
+def test_num_islands():
+    solutions = [
+        num_islands_dfs,
+    ]
+
+    grid_1 = [
+        ['0', '1', '1', '1', '0'],
+        ['0', '1', '0', '1', '0'],
+        ['1', '1', '0', '0', '0'],
+        ['0', '0', '0', '0', '0'],
+    ]
+    test_cases = [
+        (grid_1, 1),
+    ]
+
+    for solution in solutions:
+        for grid, expected_num_islands in test_cases:
+            # Act
+            num_islands = solution(grid)
+
+            # Assert
+            assert num_islands == expected_num_islands
+
+        print(f'Tests passed for {solution.__name__}!')
+
+
+if __name__ == '__main__':
+    test_num_islands()
