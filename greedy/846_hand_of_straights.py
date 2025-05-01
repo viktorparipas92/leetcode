3d85e1fd4f8a0f03c4c1b701d345e8758ae97713
Paripás Viktor <viktor.paripas@kognity.com>
2025-05-01 10:59:22 +0200

    Add hash-map based solutions to 846. Hand of Straights

--- greedy/846_hand_of_straights.py
+++ greedy/846_hand_of_straights.py
@@ -29,7 +29,7 @@ Constraints:
 1 <= groupSize <= hand.length
 """
 import heapq
-from collections import Counter
+from collections import Counter, deque
 
 
 def is_n_straight_hand_sorting(hand: list[int], group_size: int) -> bool:
@@ -80,10 +80,68 @@ def is_n_straight_hand_heap(hand: list[int], group_size: int) -> bool:
     return True
 
 
+def is_n_straight_hand_ordered_map(hand: list[int], group_size: int) -> bool:
+    """
+    Time complexity: O(n log n)
+    Space complexity: O(n)
+    """
+    if len(hand) % group_size:
+        return False
+
+    counts = Counter(hand)
+    queue = deque()
+    last_number, num_open_groups = -1, 0
+
+    for unique_number in sorted(counts):
+        if (
+            (num_open_groups > 0 and unique_number > last_number + 1)
+            or num_open_groups > counts[unique_number]
+        ):
+            return False
+
+        queue.append(counts[unique_number] - num_open_groups)
+        last_number = unique_number
+        num_open_groups = counts[unique_number]
+
+        if len(queue) == group_size:
+            num_open_groups -= queue.popleft()
+
+    return num_open_groups == 0
+
+
+def is_n_straight_hand_hash_map(hand: list[int], group_size: int) -> bool:
+    """
+    Time complexity: O(n)
+    Space complexity: O(n)
+    """
+    if len(hand) % group_size:
+        return False
+
+    counts = Counter(hand)
+    for number in hand:
+        start_number = number
+        while counts[start_number - 1]:
+            start_number -= 1
+
+        while start_number <= number:
+            while counts[start_number]:
+                for n in range(start_number, start_number + group_size):
+                    if not counts[n]:
+                        return False
+
+                    counts[n] -= 1
+
+            start_number += 1
+
+    return True
+
+
 def test_is_n_straight_hand():
     solutions = [
         is_n_straight_hand_sorting,
         is_n_straight_hand_heap,
+        is_n_straight_hand_ordered_map,
+        is_n_straight_hand_hash_map,
     ]
 
     test_cases = [
