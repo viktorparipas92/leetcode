b3c9fceac90b881293baa6672bdde6ddc2644c69
Paripás Viktor <viktor.paripas@kognity.com>
2025-06-11 23:58:25 +0200

    Add recursive solution for 91. Decode ways

--- dynamic_programming/91_decode_ways.py
+++ dynamic_programming/91_decode_ways.py
@@ -10,30 +10,52 @@ the following mapping:
 'B' -> "2"
 ...
 'Z' -> "26"
-To decode a message, digits must be grouped and then mapped back into letters using the reverse of the mapping above. There may be multiple ways to decode a message. For example, "1012" can be mapped into:
-
+To decode a message, digits must be grouped and then mapped back into letters using
+the reverse of the mapping above. There may be multiple ways to decode a message.
+For example, "1012" can be mapped into:
 "JAB" with the grouping (10 1 2)
 "JL" with the grouping (10 12)
-The grouping (1 01 2) is invalid because 01 cannot be mapped into a letter since it contains a leading zero.
+The grouping (1 01 2) is invalid because 01 cannot be mapped into a letter since it
+contains a leading zero.
 
-Given a string s containing only digits, return the number of ways to decode it. You can assume that the answer fits in a 32-bit integer.
+Given a string s containing only digits, return the number of ways to decode it.
+You can assume that the answer fits in a 32-bit integer.
 
 Example 1:
-
 Input: s = "12"
-
 Output: 2
-
 Explanation: "12" could be decoded as "AB" (1 2) or "L" (12).
+
 Example 2:
-
 Input: s = "01"
-
 Output: 0
 Explanation: "01" cannot be decoded because "01" cannot be mapped into a letter.
 
 Constraints:
-
 1 <= s.length <= 100
 s consists of digits
 """
+
+
+def num_decodings_recursion(message: str) -> int:
+    """
+    Time complexity: O(2^n)
+    Space complexity: O(n)
+    """
+
+    def depth_first_search(i: int) -> int:
+        if i == len(message):
+            return 1
+
+        char = message[i]
+        if char == '0':
+            return 0
+
+        num_decodings = depth_first_search(i + 1)
+        if i < len(message) - 1:
+            if char == '1' or (char == '2' and message[i + 1] < '7'):
+                num_decodings += depth_first_search(i + 2)
+
+        return num_decodings
+
+    return depth_first_search(0)
