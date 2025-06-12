d8df77208050209f8482ff2258d855afa422a8c8
Paripás Viktor <viktor.paripas@kognity.com>
2025-06-12 18:09:36 +0200

    Solve Meeting Rooms II. with a min-heap

---
+++ intervals/meeting_rooms_2.py
@@ -0,0 +1,71 @@
+"""
+Meeting Rooms II
+-----------------
+Difficulty: Medium
+
+Given an array of meeting time interval objects consisting of start and end times
+[[start_1,end_1],[start_2,end_2],...] (start_i < end_i),
+find the minimum number of days required to schedule all meetings without any conflicts.
+
+Note: (0,8),(8,10) is not considered a conflict at 8.
+
+Example 1:
+Input: intervals = [(0,40),(5,10),(15,20)]
+Output: 2
+Explanation:
+day1: (0,40)
+day2: (5,10),(15,20)
+
+Example 2:
+Input: intervals = [(4,9)]
+Output: 1
+
+Constraints:
+0 <= intervals.length <= 500
+0 <= intervals[i].start < intervals[i].end <= 1,000,000
+"""
+import heapq
+
+from intervals.shared import Interval
+
+
+def min_meeting_rooms_min_heap(intervals: list[Interval]) -> int:
+    """
+    Time complexity: O(n * log(n))
+    Space complexity: O(n)
+    """
+    end_times: list[int] = []
+
+    intervals.sort(key=lambda x: x.start)
+    for interval in intervals:
+        if end_times and end_times[0] <= interval.start:
+            heapq.heappop(end_times)
+
+        heapq.heappush(end_times, interval.end)
+
+    return len(end_times)
+
+
+def test_min_meeting_rooms():
+    solutions = [
+        min_meeting_rooms_min_heap,
+    ]
+
+    test_cases = [
+        ([Interval(0, 40), Interval(5, 10), Interval(15, 20)], 2),
+        ([Interval(4, 9)], 1),
+    ]
+
+    for solution in solutions:
+        for intervals, expected_min_num_days in test_cases:
+            # Act
+            min_num_days = solution(intervals)
+
+            # Assert
+            assert min_num_days == expected_min_num_days
+
+        print(f'Tests passed for {solution.__name__}!')
+
+
+if __name__ == '__main__':
+    test_min_meeting_rooms()
