9361e06fec0f0f7fd4fc613ee93619fb9c166f75
Paripás Viktor <viktor.paripas@kognity.com>
2025-06-14 23:50:20 +0200

    Solve 152. Maximum product subarray with brute force

---
+++ dynamic_programming/152_maximum_product_subarray.py
@@ -0,0 +1,64 @@
+"""
+152. Maximum Product Subarray
+-----------------------------
+Difficulty: Medium
+
+Given an integer array nums, find a subarray that has the largest product
+within the array and return it.
+A subarray is a contiguous non-empty sequence of elements within an array.
+You can assume the output will fit into a 32-bit integer.
+
+Example 1:
+Input: nums = [1,2,-3,4]
+Output: 4
+
+Example 2:
+Input: nums = [-2,-1]
+Output: 2
+
+Constraints:
+1 <= nums.length <= 1000
+-10 <= nums[i] <= 10
+"""
+
+
+def max_product_brute_force(numbers: list[int]) -> int:
+    """
+    Time complexity: O(n^2)
+    Space complexity: O(1)
+    """
+    product  = numbers[0]
+
+    for i, number in enumerate(numbers):
+        current_product = number
+        product = max(product, current_product)
+        for j, other_number in enumerate(numbers[i + 1:], start=i + 1):
+            current_product *= other_number
+            product = max(product, current_product)
+
+    return product
+
+
+def test_max_product():
+    solutions = [
+        max_product_brute_force,
+    ]
+
+    test_cases = [
+        ([1, 2, -3, 4], 4),
+        ([-2, -1], 2),
+    ]
+
+    for solution in solutions:
+        for numbers, expected_product in test_cases:
+            # Act
+            product = solution(numbers)
+
+            # Assert
+            assert product == expected_product
+
+        print(f'Tests passed for {solution.__name__}!')
+
+
+if __name__ == '__main__':
+    test_max_product()
