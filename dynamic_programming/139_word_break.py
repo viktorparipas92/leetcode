6317f3597ff16d6b991dd418f72f4df19eeef9f9
Paripás Viktor <viktor.paripas@kognity.com>
2025-06-25 01:11:40 +0200

    Solve 139. Word Break with recursion (DFS)

---
+++ dynamic_programming/139_word_break.py
@@ -0,0 +1,82 @@
+"""
+139. Word Break
+---------------
+Difficulty: Medium
+
+Given a string s and a dictionary of strings wordDict,
+return true if s can be segmented into a space-separated sequence of dictionary words.
+
+You are allowed to reuse words in the dictionary an unlimited number of times.
+You may assume all dictionary words are unique.
+
+Example 1:
+Input: s = "neetcode", wordDict = ["neet","code"]
+Output: true
+Explanation: Return true because "neetcode" can be split into "neet" and "code".
+
+Example 2:
+Input: s = "applepenapple", wordDict = ["apple","pen","ape"]
+Output: true
+Explanation: Return true because "applepenapple" can be split into "apple",
+"pen" and "apple". Notice that we can reuse words and also not use all the words.
+
+Example 3:
+Input: s = "catsincars", wordDict = ["cats","cat","sin","in","car"]
+Output: false
+
+Constraints:
+1 <= s.length <= 200
+1 <= wordDict.length <= 100
+1 <= wordDict[i].length <= 20
+s and wordDict[i] consist of only lowercase English letters.
+"""
+
+
+def word_break_recursion(target: str, words: set[str]) -> bool:
+    """
+    Time complexity: O(t * m^n)
+    Space complexity: O(n)
+    where
+    - n is the length of the target string,
+    - m is the number of words in the dictionary,
+    - t is the maximum length of a word in the dictionary.
+    """
+    def depth_first_search(i: int):
+        if i == len(target):
+            return True
+
+        return any(
+            (
+                (i + len(word)) <= len(target)
+                and target[i: i + len(word)] == word
+                and depth_first_search(i + len(word))
+            )
+            for word in words
+        )
+
+    return depth_first_search(i=0)
+
+
+def test_word_break():
+    solutions = [
+        word_break_recursion,
+    ]
+
+    test_cases = [
+        ('neetcode', {'neet', 'code'}, True),
+        ('applepenapple', {'apple', 'pen', 'ape'}, True),
+        ('catsincars', {'cats', 'cat', 'sin', 'in', 'car'}, False),
+    ]
+
+    for solution in solutions:
+        for target, words, expected_can_split in test_cases:
+            # Act
+            can_split = solution(target, words)
+
+            # Assert
+            assert can_split == expected_can_split
+
+        print(f'Tests passed for {solution.__name__}!')
+
+if __name__ == '__main__':
+    test_word_break()
