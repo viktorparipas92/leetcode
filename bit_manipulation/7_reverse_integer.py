6d6c2291659c995f7423d92b4c04dd415741a69f
Paripás Viktor <viktor.paripas@kognity.com>
2025-07-05 12:55:25 +0200

    Solve 7. Reverse Integer with brute force and recursion

---
+++ bit_manipulation/7_reverse_integer.py
@@ -0,0 +1,91 @@
+"""
+7. Reverse Integer
+-----------------
+Difficulty: Medium
+
+You are given a signed 32-bit integer x.
+Return x after reversing each of its digits.
+After reversing, if x goes outside the signed 32-bit integer range [-2^31, 2^31 - 1],
+then return 0 instead.
+Solve the problem without using integers that are outside the signed 32-bit integer range.
+
+Example 1:
+Input: x = 1234
+Output: 4321
+
+Example 2:
+Input: x = -1234
+Output: -4321
+
+Example 3:
+Input: x = 1234236467
+Output: 0
+
+Constraints:
+-2^31 <= x <= 2^31 - 1
+"""
+
+def reverse_brute_force(x: int) -> int:
+    """
+    Time complexity: O(1)
+    Space complexity: O(1)
+    """
+    absolute = abs(x)
+    reverse: int = int(str(absolute)[::-1])
+    if x < 0:
+        reverse *= -1
+
+    if reverse < -(1 << 31) or reverse > (1 << 31) - 1:
+        return 0
+
+    return reverse
+
+
+def reverse_recursion(x: int) -> int:
+    """
+    Time complexity: O(1)
+    Space complexity: O(1)
+    """
+
+    def recurse(n: int, reverse: int) -> int:
+        if n == 0:
+            return reverse
+
+        reverse = reverse * 10 + n % 10
+        return recurse(n // 10, reverse)
+
+    sign = -1 if x < 0 else 1
+    absolute = abs(x)
+    reversed_number = recurse(n=absolute, reverse=0)
+    reversed_number *= sign
+    if reversed_number < -(1 << 31) or reversed_number > (1 << 31) - 1:
+        return 0
+
+    return reversed_number
+
+
+def test_reverse_integer():
+    solutions = [
+        reverse_brute_force,
+        reverse_recursion,
+    ]
+
+    test_cases = [
+        (1234, 4321),
+        (-1234, -4321),
+        (1234236467, 0),
+    ]
+
+    for solution in solutions:
+        for x, expected_reverse in test_cases:
+            # Act
+            reversed_integer = solution(x)
+
+            # Assert
+            assert reversed_integer == expected_reverse
+
+        print(f'Tests passed for {solution.__name__}!')
+
+
+if __name__ == '__main__':
+    test_reverse_integer()
