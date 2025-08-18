95676e59cc5ac5972912dfa00df0036a177d10b7
Paripás Viktor <viktor.paripas@kognity.com>
2025-08-18 14:11:56 +0200

    Solve 127. Word Ladder with BFS - meet in the middle2

--- graphs/127_word_ladder.py
+++ graphs/127_word_ladder.py
@@ -67,9 +67,53 @@ def ladder_length_breadth_first_search(begin_word: str, end_word: str, word_list
     return 0
 
 
+def ladder_length_meet_in_the_middle_bfs(
+    begin_word: str,
+    end_word: str,
+    word_list: list[str],
+) -> int:
+    if end_word not in word_list or begin_word == end_word:
+        return 0
+
+    word_length: int = len(word_list[0])
+
+    words: set[str] = set(word_list)
+
+    queue_beginning = deque([begin_word])
+    from_beginning: dict[str, int] = {begin_word: 1}
+
+    queue_end = deque([end_word])
+    from_end: dict[str, int] = {end_word: 1}
+
+    while queue_beginning and queue_end:
+        if len(queue_beginning) > len(queue_end):
+            queue_beginning, queue_end = queue_end, queue_beginning  # Swap to always expand the smaller queue
+            from_beginning, from_end = from_end, from_beginning
+
+        for _ in range(len(queue_beginning)):
+            left_word = queue_beginning.popleft()
+            num_steps: int = from_beginning[left_word]
+            for i in range(word_length):
+                for char in string.ascii_lowercase:
+                    if char == left_word[i]:
+                        continue
+
+                    neighbour: str = left_word[:i] + char + left_word[i + 1:]
+                    if neighbour not in words:
+                        continue
+                    elif neighbour in from_end:
+                        return num_steps + from_end[neighbour]
+                    elif neighbour not in from_beginning:
+                        from_beginning[neighbour] = num_steps + 1
+                        queue_beginning.append(neighbour)
+
+    return 0
+
+
 def test_ladder_length():
     solutions = [
         ladder_length_breadth_first_search,
+        ladder_length_meet_in_the_middle_bfs,
     ]
 
     test_cases = [
