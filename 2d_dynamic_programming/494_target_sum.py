1973eca2a453abcb9eeedf02f7d99affa0c23abc
Paripás Viktor <viktor.paripas@kognity.com>
2025-09-30 23:37:34 +0200

    Solve 494. Target Sum with recursion

---
+++ 2d_dynamic_programming/494_target_sum.py
@@ -0,0 +1,69 @@
+"""
+494. Target Sum
+----------------------------
+Difficulty: Medium
+
+You are given an array of integers nums and an integer target.
+For each number in the array, you can choose to either add
+or subtract it to a total sum.
+
+For example, if nums = [1, 2], one possible sum would be "+1-2=-1".
+If nums=[1,1], there are two different ways to sum the input numbers
+to get a sum of 0: "+1-1" and "-1+1".
+
+Return the number of different ways that you can build the expression
+such that the total sum equals target.
+
+Example 1:
+Input: nums = [2,2,2], target = 2
+Output: 3
+Explanation: There are 3 different ways to sum the input numbers to get a sum of 2.
++2 +2 -2 = 2
++2 -2 +2 = 2
+-2 +2 +2 = 2
+
+Constraints:
+1 <= nums.length <= 20
+0 <= nums[i] <= 1000
+-1000 <= target <= 1000
+"""
+
+
+def find_target_sum_ways_recursion(numbers: list[int], target: int) -> int:
+    """
+    Time complexity: O(2^n)
+    Space complexity: O(n)
+    where n is the length of numbers.
+    """
+    def backtrack(i, total):
+        if i == len(numbers):
+            return  total == target
+
+        number = numbers[i]
+        return backtrack(i + 1, total + number) + backtrack(i + 1, total - number)
+
+    return backtrack(i=0, total=0)
+
+
+def test_find_target_sum_ways():
+    solutions = [
+        find_target_sum_ways_recursion,
+    ]
+
+    test_cases = [
+        ([2, 2, 2], 2, 3),
+    ]
+
+    for solution in solutions:
+        for numbers, target, expected_num_ways in test_cases:
+            # Act
+            num_ways = solution(numbers, target)
+
+            # Assert
+            assert num_ways == expected_num_ways
+
+        print(f'{solution.__name__}: All test cases pass')
+
+
+if __name__ == '__main__':
+    test_find_target_sum_ways()
