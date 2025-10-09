1c3ae4d8c6066adbf108fa6f6c6464c47f698c56
Paripás Viktor <viktor.paripas@kognity.com>
2025-10-09 02:15:01 +0200

    Solve 10. Regex Matching with recursion

---
+++ 2d_dynamic_programming/10_regex_matching.py
@@ -0,0 +1,88 @@
+"""
+10. Regular Expression Matching
+------------------------------
+Difficulty: Hard
+
+You are given an input string s consisting of lowercase english letters,
+ and a pattern p consisting of lowercase english letters,
+ as well as '.', and '*' characters.
+
+Return true if the pattern matches the entire input string, otherwise return false.
+- '.' Matches any single character
+- '*' Matches zero or more of the preceding element.
+
+Example 1:
+Input: s = "aa", p = ".b"
+Output: false
+Explanation: Regardless of which character we choose for the '.' in the pattern, we cannot match the second character in the input string.
+
+Example 2:
+Input: s = "nnn", p = "n*"
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'n'. We choose 'n' to repeat three times.
+
+Example 3:
+Input: s = "xyz", p = ".*z"
+Output: true
+Explanation: The pattern ".*" means zero or more of any character, so we choose ".." to match "xy" and "z" to match "z".
+
+Constraints:
+1 <= s.length <= 20
+1 <= p.length <= 20
+Each appearance of '*', will be preceded by a valid character or '.'
+"""
+
+
+def is_match_recursion(string: str, pattern: str) -> bool:
+    """
+    Time complexity: O(2^(m+n))
+    Space complexity: O(m+n)
+    where m is the length of the string and n is the length of the pattern.
+    """
+    def depth_first_search(i: int, j: int) -> bool:
+        if j == pattern_size:
+            return i == string_size
+
+        match = (
+            i < string_size
+            and (string[i] == pattern[j] or pattern[j] == '.')
+        )
+        if (j + 1) < pattern_size and pattern[j + 1] == '*':
+            return (
+                depth_first_search(i, j + 2)  # don't use *
+                or (match and depth_first_search(i + 1, j))  # use *
+            )
+        elif match:
+            return depth_first_search(i + 1, j + 1)
+
+        return False
+
+    string_size = len(string)
+    pattern_size = len(pattern)
+    return depth_first_search(i=0, j=0)
+
+
+def test_is_match():
+    solutions = [
+        is_match_recursion,
+    ]
+
+    test_cases = [
+        ('aa', '.b', False),
+        ('nnn', 'n*', True),
+        ('xyz', '.*z', True),
+    ]
+
+    for solution in solutions:
+        for string, pattern, expected_is_match in test_cases:
+            # Act
+            is_match = solution(string, pattern)
+
+            # Assert
+            assert is_match == expected_is_match
+
+        print(f'Tests passed for {solution.__name__}!')
+
+
+if __name__ == '__main__':
+    test_is_match()
